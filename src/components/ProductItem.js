a2f14c85fc0957acc70125f317c17659ecb87150
naufalf25 <opal.farras@gmail.com>
2023-02-08 12:23:25 +0700

    List products added

---
+++ src/components/ProductItem.js
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow w-full"
+          width={600}
+          height={600}
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import Layout from "@/components/Layout";
+import ProductItem from "@/components/ProductItem";
+import data from "@/utils/data";
 
 export default function Home() {
   return (
     <Layout title="Home">
-      <h1 className="text-xl font-bold">Home Page</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
     </Layout>
   )
 }
